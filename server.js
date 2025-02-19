6a3173af73b5ffe26c68821b1084276a76b1ff9e
BaltiBlast <fougeray.florian@gmail.com>
2025-02-19 13:21:45 +0100

    express router init

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+// NPM import //
 const express = require("express");
+
+// local import //
 const { attachNikon } = require("./utils/methods");
+const router = require("./router");
 
 const app = express();
 const PORT = 3000;
 
 attachNikon();
 
+app.use(router);
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
