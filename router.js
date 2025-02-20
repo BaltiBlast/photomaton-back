57156afdaed35774ef6f69b128f1c0586ad52e49
BaltiBlast <fougeray.florian@gmail.com>
2025-02-20 16:10:57 +0100

    form to add email adress user to google drive folder

--- router.js
+++ router.js
@@ -1,9 +1,16 @@
 // NPM import //
 const express = require("express");
+const { google } = require("googleapis");
 const { exec } = require("child_process");
+require("dotenv").config();
 
 // local import //
 const { getFileName } = require("./utils/methods");
+const credentials = require("./credentials.json");
+const auth = new google.auth.GoogleAuth({
+  credentials,
+  scopes: ["https://www.googleapis.com/auth/drive"],
+});
 
 const router = express.Router();
 
@@ -25,6 +32,34 @@ router.get("/capture", (req, res) => {
       res.json({ imageUrl: `/photos/${fileName}` });
     }
   );
+});
+
+router.post("/add-to-drive", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const client = await auth.getClient();
+    const drive = google.drive({ version: "v3", auth: client });
+
+    // ID du dossier Google Drive où vous souhaitez ajouter l'utilisateur
+    const folderId = process.env.GOOGLE_DRIVE_FOLDER_ID;
+
+    // Ajouter l'utilisateur au dossier
+    await drive.permissions.create({
+      fileId: folderId,
+      requestBody: {
+        role: "reader", // Ou 'reader' si vous voulez seulement permettre la lecture
+        type: "user",
+        emailAddress: email,
+      },
+    });
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Erreur :", error);
+    console.error("Détails de l'erreur :", error.response); // <-- Ajoute ceci
+    res.status(500).json({ success: false, message: error.message });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = 3000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
 attachNikon();
 
